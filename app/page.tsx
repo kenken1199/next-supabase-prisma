1d913bba4e3ded8a1ffb658c4c2291f70595b218
kenken1199 <nakamori.k@gmail.com>
2023-06-06 18:24:13 +0900

    no revalidate

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import prisma from "../lib/prisma";
 
-export const revalidate = 3600; // revalidate every hour
+// export const revalidate = 3600; // revalidate every hour
 
 async function getCount() {
   const count = await prisma.user.count();
